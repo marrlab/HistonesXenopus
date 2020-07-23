9ce9b68524142abd28b030d094e682e3796d69ff
Lea Schuh <lea.schuh@tum.de>
2020-07-23 17:00:54 +0200

    Update plotUncertainty.m

--- functions/plotUncertainty.m
+++ functions/plotUncertainty.m
@@ -5,17 +5,17 @@ load('./results/MCMCAll')
 MAll = cell(6,1);
 MAll{2} = MCMCAll{1};
 MAll{3} = MCMCAll{3};
-MAll{5} = MCMCAll{2};
+MAll{4} = MCMCAll{2};
 MAll{6} = MCMCAll{4};
 
 load('./results/MCMCAll_joint_nodemmock_demHUA')
 MAll{1} = MCMCAll{1};
-MAll{4} = MCMCAll{2};
+MAll{5} = MCMCAll{2};
 
-Mpar = [6,9,6,10,6,10,6,9,6,10,6,10;... %me1m and me1h for models a-f
-    7,10,7,11,7,11,7,10,7,11,7,11;...  %me2m and me2h for models a-f
-    8,NaN,8,NaN,8,NaN,8,11,8,12,8,12;...  %me3m and me3h for models a-f
-    NaN,11,9,NaN,9,12,NaN,12,9,NaN,9,13];  %dm and dh for models a-f
+Mpar = [6,9,6,10,6,10,6,10,6,9,6,10;... %me1m and me1h for models a-f
+    7,10,7,11,7,11,7,11,7,10,7,11;...  %me2m and me2h for models a-f
+    8,NaN,8,NaN,8,NaN,8,12,8,11,8,12;...  %me3m and me3h for models a-f
+    NaN,11,9,NaN,9,12,9,NaN,NaN,12,9,13];  %dm and dh for models a-f
 
 Mmodel = [1,1,2,2,3,3,4,4,5,5,6,6];
 
