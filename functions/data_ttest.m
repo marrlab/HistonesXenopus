e96b20e96a1ac1c5623bc32940c40afa9f2473ef
Lea Schuh <lea.schuh@tum.de>
2020-07-23 17:00:38 +0200

    Create data_ttest.m

---
+++ functions/data_ttest.m
@@ -0,0 +1,22 @@
+clearvars;
+clc;
+
+addpath(genpath('../HistonesXenopus')) 
+H4K20dummy_import;
+
+count = 1;
+for i = 1:size(DA(1).y,1)
+   for j =  1:size(DA(1).y,2)
+       clearvars a b
+       for irep = 1:length(DA)
+           a(irep) = exp(DA(irep).y(j,i));
+           b(irep) = exp(DB(irep).y(j,i));
+       end
+       [h,p] = ttest2(a,b);
+       H(count) = h;
+       P(count) = p;
+       count = count+1;
+   end
+end
+
+length(find(P<0.05))
